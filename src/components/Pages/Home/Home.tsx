c79ca1e519bf8d531f2fce86df36e91b196cd21b
NhatNC <NhatNC@macbook-air.local>
2023-12-14 22:40:53 +0700

    fix: switch poll answered vs unanswered

--- src/components/Pages/Home/Home.tsx
+++ src/components/Pages/Home/Home.tsx
@@ -3,11 +3,13 @@ import { useSelector } from "react-redux";
 import { getQuestions, getUser } from "../../../plugins/store";
 import { useNavigate } from "react-router-dom";
 import "./Home.scss"
+import { useState } from "react";
 
 export default function Home() {
     const navigate = useNavigate();
     const questions = useSelector(getQuestions);
     const user = useSelector(getUser);
+    const [viewMode, setViewMode] = useState<"answered" | "unanswered">("unanswered");
 
     const answered = Object.keys(questions).filter(question => Object.keys(user.answers).includes(question))
         .map(id => questions[id]);
@@ -18,10 +20,17 @@ export default function Home() {
         navigate(`/questions/${id}`);
     }
 
+    function handleChangeViewMode(): void {
+        setViewMode(viewMode === "answered" ? "unanswered" : "answered");
+    }
+
     return (
-        <>
-            <PollShell headline="Poll Unanswered" polls={unanswered} onView={handleView} />
-            <PollShell headline="Poll Answered" polls={answered} onView={handleView} />
-        </>
+        <div className="home-page">
+            <button type="button" className="switch-mode" onClick={handleChangeViewMode}>{viewMode === "answered" ? "Unanswered" : "Answered"}</button>
+            {viewMode === "unanswered" &&
+                <PollShell headline="Poll Unanswered" polls={unanswered} onView={handleView} />}
+            {viewMode === "answered" &&
+                <PollShell headline="Poll Answered" polls={answered} onView={handleView} />}
+        </div>
     );
 }
